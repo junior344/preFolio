8ee4ea9d894d9aa4f44dbe5ed5db36ad97f4005d
josias <josiasmbogle237@gmail.com>
2025-01-07 15:08:05 +0100

    section projet

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_outward" />
     <link rel="stylesheet" href="style.css">
     <title>portfolio</title>
 </head>
 <body>
     <?php include 'pages/header.php'; ?>
 
-    <div class=" d-flex justify-content-center flex-column m-5 ">
+    <div class=" d-flex justify-content-center flex-column m-5 p-5 ">
         <h1 class=" fw-bolder custom-font-size ">Josias Junior Mbogle</h1>
         <p class="fs-1">Developpeur Full-Stack</p>
         <p class="fs-5 mt-3">
@@ -21,8 +22,8 @@
             Je m'efforce de rester à jour avec les dernières tendances et innovations dans le domaine du développement.
         </p>
     </div>
-    <div class="TOOLS m-5 d-flex">
-        <span class="badge rounded-pill text-bg-primary m-1">TYPESCRIPT</span>
+    <div class="TOOLS m-5 p-5 d-flex">
+        <span class="badge rounded-pill text-bg-primary  m-1">TYPESCRIPT</span>
         <span class="badge rounded-pill text-bg-secondary m-1">JAVASCRIPT</span>
         <span class="badge rounded-pill text-bg-success m-1">REACT</span>
         <span class="badge rounded-pill text-bg-danger m-1">SASS/SCSS</span>
@@ -32,6 +33,10 @@
         <span class="badge rounded-pill text-bg-dark m-1">NODEJS</span>
     </div>
 
+    <main class="bg-dark ">
+        <h2 class="p-5 text-light text-center">PROJETS</h2>
+        <?php include 'pages/projet.php'; ?>
+    </main>
 
 
     <?php include 'pages/footer.php'; ?>
--- pages/header.php
+++ pages/header.php
@@ -1,7 +1,7 @@
 
-<header class="bg-dark text-light d-flex flex-wrap justify-content-center p-3 mb-4 border-bottom">
+<header class="bg-dark text-light d-flex flex-wrap justify-content-center p-3 mb-4 border-bottom fixed-top">
 
-      <ul class="nav nav-pills">
+      <ul class="nav nav-pills ">
         <li class="nav-item "><a href="#" class="nav-link active" >Home</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Projet</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
